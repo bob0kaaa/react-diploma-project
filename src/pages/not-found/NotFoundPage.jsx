ea49d68a2d5cd60c95340178932597708b2f55d4
alidf <d.alimov@it-element29.tech>
2026-06-04 14:13:53 +0500

    fix: структура AboutPage и NotFoundPage по HTML-шаблонам

--- src/pages/not-found/NotFoundPage.jsx
+++ src/pages/not-found/NotFoundPage.jsx
@@ -1,12 +1,14 @@
 export default function NotFoundPage() {
     return (
-        <div className="row">
-            <div className="col">
-                <section className="top-sales">
-                    <h2 className="text-center">Страница не найдена</h2>
-                    <p className="text-center">Извините, такая страница не найдена!</p>
-                </section>
+        <main className="container">
+            <div className="row">
+                <div className="col">
+                    <section className="top-sales">
+                        <h2 className="text-center">Страница не найдена</h2>
+                        <p>Извините, такая страница не найдена!</p>
+                    </section>
+                </div>
             </div>
-        </div>
+        </main>
     );
 }
